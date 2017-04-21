0c83d1e0f20ab96c7728d1b2bd63a0960dbbf549
Lorenzo Gaborini <lorenzo.gaborini@unil.ch>
2017-04-21 17:14:03 +0200

    Added default messages for events.

--- events.py
+++ events.py
@@ -11,33 +11,43 @@ class HanabiError(Exception):
 class CardError(HanabiError):
     """A pile is emptied: e.g. no more card in hand."""
     
-    def __init__(self, msg):
+    def __init__(self, msg = None):
+        if msg is None:
+            msg = "No more cards in this stack."
         super().__init__(msg, is_game_error = False)
         
         
 class EmptyDeck(HanabiError):
     """A deck is finished. Triggers last turn."""
     
-    def __init__(self, msg):
+    def __init__(self, msg = None):
+        if msg is None:
+            msg = "No more cards in deck."
         super().__init__(msg, is_game_error = False)
 
 
 class InvalidActionError(HanabiError):
     """An invalid Hanabi action. Does not decrement the error counter."""
     
-    def __init__(self, msg):
+    def __init__(self, msg = None):
+        if msg is None:
+            msg = "Invalid Hanabi action."
         super().__init__(msg, is_game_error = False)
         
         
 class WrongPlayError(HanabiError):
     """A wrong play. Decrements the error counter, however it is a valid action."""
-    def __init__(self, msg):
+    def __init__(self, msg = None):
+        if msg is None:
+            msg = "Wrong Hanabi action!"
         super().__init__(msg, is_game_error = True)
      
         
 class CompletedPile(HanabiError):
     """A pile has been completed."""
     
-    def __init__(self, msg):
+    def __init__(self, msg = None):
+        if msg is None:
+            msg = "A pile has been completed."
         super().__init__(msg, is_game_error = False)
     
